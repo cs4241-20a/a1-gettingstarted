b1d1b3eeb0564959809136f4b3bc92cb769c382e
Ivan <ieroshenko@wpi.edu>
2020-09-01 21:58:50 -0400

    the website part is done. Now off to submissions

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 const COLORS = ["#4578C6", "#5797F7", "#4578C4"];
+const RADIUSES = ["10px", "20px", "30px", "40px", "50px"];
+const headerTxt = document.getElementById("main-txt");
+const elem = document.getElementById("elem");
 
 function getRandomInt(max) {
     return Math.floor(Math.random() * Math.floor(max));
 }
 
-function changeColor() {
-    let headerTxt = document.getElementById("main-txt");
+function animate() {
     headerTxt.style.color = COLORS[getRandomInt(3)];
+    elem.style.borderRadius= RADIUSES[getRandomInt(5)];
 }
 
-setInterval(changeColor, 2000)
+setInterval(animate, 1000)
