6ce92c35107d3655ec1bf75cb17853ab5fcd7380
Clay OL <clay.oshiroleavitt@gmail.com>
2020-09-03 15:06:21 -0400

    updated index.html to conform to requirements, added CSS styling and additional image files, updated server to support new files, updated readme to indicate the work I've done

--- server.js
+++ server.js
@@ -10,6 +10,12 @@ const server = http.createServer( function( request,response ) {
     case '/index.html':
       sendFile( response, 'index.html' )
       break
+    case '/CSS/style.css':
+      sendFile( response, 'CSS/style.css' )
+      break
+    case '/Images/profile.png':
+      sendFile( response, 'Images/profile.png' )
+      break
     default:
       response.end( '404 Error: File Not Found' )
   }
