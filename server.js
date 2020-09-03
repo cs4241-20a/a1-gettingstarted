0e2be95079e1f403fea181486f4d3d9ced4aff1b
mollywirtz <mmwirtz@wpi.edu>
2020-09-03 14:46:42 -0400

    Fix server to handle imgs, css

--- server.js
+++ server.js
@@ -3,15 +3,35 @@ const http = require('http'),
       port = 3000
 
 const server = http.createServer( function( request,response ) {
+
   switch( request.url ) {
+    
     case '/':
+      console.log(request.url)
       sendFile( response, 'index.html' )
       break
+    
     case '/index.html':
       sendFile( response, 'index.html' )
       break
+    
+    case '/style.css':
+      sendFile(response, 'style.css')
+      break
+
+    case '/assets/linkedin.png':
+      sendFile(response, "assets/linkedin.png")
+      break
+  
+    case '/assets/github.png':
+      sendFile(response, "assets/github.png")
+      break
+
     default:
       response.end( '404 Error: File Not Found' )
+      console.log("here?")
+      //sendFile( response, request.url )
+      break
   }
 })
 
@@ -19,6 +39,11 @@ server.listen( process.env.PORT || port )
 
 const sendFile = function( response, filename ) {
    fs.readFile( filename, function( err, content ) {
+     if (err) {
+      response.end( '404 Error: File Not Found' )
+      console.log("or here?")
+      //sendFile( response, request.url )
+     }
      file = content
      response.end( content, 'utf-8' )
    })
