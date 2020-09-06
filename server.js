6b61ee8e9c08cfbcebb9b5fbedc0d48d646ea81b
TweedChristian <christiantweed@gmail.com>
2020-09-06 15:20:18 -0400

    Wrote a memoized file selector and started the error file

--- server.js
+++ server.js
@@ -1,6 +1,52 @@
 const http = require('http'),
       fs   = require('fs'),
       port = 3000
+ 
+
+
+const memoizedFs = (response) => {
+  let cache = {};
+  return (fileName) => {
+    if(fileName in cache) {
+      let fileString = cache[fileName];
+      if(!!response){
+        response.end(fileString, 'utf-8');
+      }
+    }
+    fs.readFile(fileName, 'utf8', (err, data) => {
+      if(err){
+        if(!!cache['error.html'] && !!response){
+          response.end(cache['error.html'], 'utf-8');
+        }
+        else if(!!response){
+          //If for some reason fs never loaded the error response
+          response.end( '404 Error: File Not Found' )
+        }
+        else{
+          //Do nothing
+        }
+      }
+      else {
+        cache[fileName] = data;
+        if(!!response) {
+          response.end(data, 'utf-8');
+        }
+      }
+    });
+  }
+}
+
+const sendFile = (response, fileName) => {
+  memoizedFs(response)(fileName);
+}
+
+const loadFile = (fileName) => {
+  memoizedFs()(fileName);
+}
+
+//Loading index and error files
+loadFile('index.html');
+loadFile('error.html');
 
 const server = http.createServer( function( request,response ) {
   switch( request.url ) {
@@ -13,13 +59,17 @@ const server = http.createServer( function( request,response ) {
     default:
       response.end( '404 Error: File Not Found' )
   }
-})
+});
+
+
 
 server.listen( process.env.PORT || port )
 
-const sendFile = function( response, filename ) {
-   fs.readFile( filename, function( err, content ) {
-     file = content
-     response.end( content, 'utf-8' )
-   })
-}
+// const sendFile = function( response, filename ) {
+//    fs.readFile( filename, function( err, content ) {
+//      file = content
+//      response.end( content, 'utf-8' )
+//    })
+// }
+
+//Based off of https://www.freecodecamp.org/news/understanding-memoize-in-javascript-51d07d19430e
