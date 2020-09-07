70636c9780836b56967e5ab773768b1fe892e8fa
Glitch (a1-cgilmore18) <none>
2020-09-07 07:14:31 +0000

    👯🍭 Updated with Glitch

---
+++ script.js
@@ -0,0 +1,9 @@
+var circle = document.getElementById('circle');
+    
+  function animate(){
+    circle.style.transform = 'translate(50px, 20px)';
+  }
+
+      if(document.getElementById('click').clicked){
+        animate();
+      }
